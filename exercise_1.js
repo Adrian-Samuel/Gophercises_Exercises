051cbd45286c016dfe173df04b6fde273fcb36a1
Adrian Samuel <31516988+Adrian-Samuel@users.noreply.github.com>
2020-03-23 01:43:28 -0400

    Create exercise_1.js

---
+++ exercise_1.js
@@ -0,0 +1,26 @@
+const {readFile} = require('fs').promises;
+
+const  parseFile = async () => {
+    const fileName = process.argv.slice(2)[0].split("=")[1];
+
+    const getFile = await readFile(fileName, "utf8");
+
+    const rows = getFile.split(/\r?\n/g);
+    const results = {right: 0, wrong:0};
+
+    for(const record of rows) {
+    const [operation, answer] = record.split(',');
+    const [firstNumber, secondNumber] = operation.split('+');
+
+    const mathExpression = Number(firstNumber) + Number(secondNumber) === Number(answer);
+
+    if(mathExpression)results["right"] += 1;
+    if(!mathExpression)results["wrong"] += 1
+
+    }
+
+    console.log(results)
+};
+
+
+parseFile();
