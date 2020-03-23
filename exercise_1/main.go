e08724b519a741beb5531d0b801a533b7f681adc
Adrian Samuel <31516988+Adrian-Samuel@users.noreply.github.com>
2020-03-23 01:45:46 -0400

    Create main.go

---
+++ exercise_1/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func returnMathTuple(mathOp string) (int64, int64) {
+	var num int64
+	var num2 int64
+
+	math := strings.Split(mathOp, "+")
+	num, err := strconv.ParseInt(math[0], 10, 64)
+	if err != nil {fmt.Println(err)}
+	num2, err = strconv.ParseInt(math[1], 10, 64)
+	if err != nil {fmt.Println(err)}
+
+	return num, num2
+}
+
+func main() {
+	fileName := flag.String("filename","","the name of the file")
+	flag.Parse()
+	content, err := ioutil.ReadFile(*fileName)
+
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	csv := string(content)
+
+	row := strings.Split(csv, "\n")
+
+	counter := map[string]int{"right": 0, "wrong": 0}
+
+	for _, value := range row {
+
+		record := strings.Split(value, ",")
+		math, answer := record[0], record[1]
+
+		answerNumber, err := strconv.ParseInt(answer, 10, 64)
+
+		if err != nil {fmt.Println(err)}
+
+		firstNumber, secondNumber := returnMathTuple(math)
+		expression:= firstNumber+secondNumber == answerNumber
+
+		if expression {counter["right"]+= 1}
+		if !expression {counter["left"]+=1}
+	}
+
+	fmt.Println(counter)
+
+}
